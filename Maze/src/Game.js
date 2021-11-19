fa82ef740836d15516beffa3d32c1b232607306a
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-19 14:00:32 +0100

    Adding messages to game syntax + creating methods pause game and continue game for easier manipulation of the game

--- Maze/src/Game.js
+++ Maze/src/Game.js
@@ -5,7 +5,7 @@ import CarCursor from "./objects/CarCursor";
 import DifficultyManager from "./objects/DifficultyManager";
 import Timer from "./objects/Timer";
 import NumberButtonManager from "./level2/NumberButtonManager";
-import {config} from "./config";
+import MessageManager from "./messages/MessageManager";
 
 let sketch;
 let grid;
@@ -14,23 +14,25 @@ let cursor;
 let difficultyManager;
 let timer;
 let numberButtons;
+let messageManager;
 
 //TODO Fetch difficulties
-//TODO align numbers in second level
 //TODO API call
-//TODO
 
 export default class Game {
     constructor(Sketch) {
         sketch = Sketch;
 
-        difficultyManager = new DifficultyManager(0, 0);
+        difficultyManager = new DifficultyManager(1, 0);
         scoreBoard = new ScoreBoard(sketch);
         grid = new Grid(sketch, difficultyManager);
         grid.setup();
         cursor = new CarCursor(sketch, grid.getCarPosition());
         timer = new Timer(sketch);
         numberButtons = new NumberButtonManager(sketch, difficultyManager);
+        messageManager = new MessageManager(sketch, grid.getGridLeftBottomPosition().y);
+
+        timer.create();
 
         //Subscribe
         grid.subscribe(this);
@@ -46,30 +48,41 @@ export default class Game {
         grid.draw();
         scoreBoard.draw();
         timer.draw();
-        cursor.draw();
         numberButtons.draw();
+        messageManager.draw();
+        cursor.draw();
     }
 
-    observerChange(action) {
+    observerChange(action, data) {
         switch (action) {
             case ObserverChange.mazeSolved:
+                this.pauseGame();
                 this.mazeFinishedHandler();
                 break;
             case ObserverChange.levelFinished:
-                this.levelFinishedHandler();
+                this.pauseGame();
+                cursor.stopCarGeneration();
+                messageManager.displayLevelFinishedMessage(() => this.levelFinishedHandler(), difficultyManager.getCurrentLevelNo());
                 break;
             case ObserverChange.dailyTimerFinished:
-                this.dailyTimerFinishedHandler();
+                this.pauseGame();
+                messageManager.displayTimeOverMessage(() => this.dailyTimerFinishedHandler());
                 break;
             case ObserverChange.levelNotPassed:
-                this.levelNotPassedHandler();
+                this.pauseGame()
+                cursor.remove();
+                messageManager.displayLevelNotPassedMessage(() => this.levelNotPassedHandler());
                 break;
             case ObserverChange.difficultyFinished:
+                this.pauseGame();
                 this.difficultyFinishedHandler();
                 break;
             case ObserverChange.gameFinished:
-                this.gameFinishedHandler();
+                this.pauseGame();
+                messageManager.displayGameFinishedMessage(() => this.gameFinishedHandler());
                 break;
+            case ObserverChange.saveDataToAPI:
+                scoreBoard.saveDataToApi(data.level, data.difficulty);
         }
         if (difficultyManager.getCurrentLevelNo() === 0) {
             switch (action) {
@@ -96,54 +109,74 @@ export default class Game {
         console.log(action);
     }
 
-    mazeFinishedHandler() {
+    pauseGame() {
+        timer.remove();
         scoreBoard.clearInterval();
+        if (difficultyManager.getCurrentLevelNo() === 0) {
+            grid.stopListening();
+        } else {
+            numberButtons.removeListeners();
+        }
+    }
+
+    continueGame() {
+        timer.create();
+        if (difficultyManager.getCurrentLevelNo() === 0) {
+            grid.startListening();
+            scoreBoard.startInterval();
+        } else {
+            numberButtons.createListeners();
+        }
+    }
+
+    mazeFinishedHandler() {
         grid.setup();
 
         if (difficultyManager.getCurrentLevelNo() === 0) {
-            timer.remove();
             cursor.remove();
             scoreBoard.resetCollisions();
             cursor.setDefaultCarPosition(grid.getCarPosition());
         } else {
             numberButtons.setCorrectCombination(grid.getCorrectCombination());
         }
+        this.continueGame();
     }
 
     startMazeRoundHandler() {
+        this.continueGame();
         cursor.create();
-        scoreBoard.startInterval();
-        timer.create();
     }
 
     dailyTimerFinishedHandler() {
-
+        this.continueGame();
     }
 
     levelNotPassedHandler() {
-
+        difficultyManager.resetDifficulty();
+        grid.gameNotStarted();
+        grid.setup();
+        if (difficultyManager.getCurrentLevelNo() === 0) {
+            scoreBoard.setDefaultLevelOneScore();
+        } else {
+            scoreBoard.setDefaultLevelTwoScore();
+            numberButtons.setCorrectCombination(grid.getCorrectCombination());
+        }
+        this.continueGame();
     }
 
     difficultyFinishedHandler() {
-        scoreBoard.saveDataToApi();
-
         if (difficultyManager.getCurrentLevelNo() === 0) {
             scoreBoard.setDefaultLevelOneScore()
         } else {
             scoreBoard.setDefaultLevelTwoScore()
         }
-
         this.mazeFinishedHandler();
     }
 
     gameFinishedHandler() {
-
     }
 
     levelFinishedHandler() {
-        scoreBoard.saveDataToApi();
-        cursor.stopCarGeneration();
-
         this.mazeFinishedHandler();
 
         if (difficultyManager.getCurrentLevelNo() === 0) {
@@ -152,12 +185,14 @@ export default class Game {
             scoreBoard.setDefaultLevelTwoScore()
             this.setLevelTwo();
         }
+        this.continueGame();
     }
 
     setLevelTwo() {
         cursor.stopCarGeneration();
         numberButtons.generate(grid.getGridLeftBottomPosition());
         numberButtons.setCorrectCombination(grid.getCorrectCombination());
-        timer.create();
+        scoreBoard.reset();
+        this.continueGame();
     }
 }
