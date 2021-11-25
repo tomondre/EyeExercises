71eeeb829c269222a0744b7ae563c27de9ae716d
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-25 16:06:26 +0100

    Adding handler for button callbacks

--- Maze/src/messages/ButtonManager.js
+++ Maze/src/messages/ButtonManager.js
@@ -1,4 +1,12 @@
 export default class ButtonManager
 {
-
+    static createMiddleButton(callback, message) {
+        let elementById = document.getElementById("middleButton");
+        elementById.style.visibility = "visible";
+        elementById.innerText = message;
+        elementById.addEventListener("click", () => {
+            elementById.style.visibility = "hidden";
+            callback();
+        });
+    }
 }
