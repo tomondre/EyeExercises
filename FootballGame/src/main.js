161c73abaee2adf33145a193d554f9fcc84c5660
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-30 17:54:52 +0100

    Football exercise now displays rotate screen message when being played on vertical

--- FootballGame/src/main.js
+++ FootballGame/src/main.js
@@ -15,6 +15,25 @@ const config = {
     scale: {mode: Phaser.Scale.FIT}
 }
 
-const game = new Phaser.Game(config);
+window.addEventListener("orientationchange", () => {
+    location.reload();
+});
+if (window.innerWidth < window.innerHeight) {
+    var canv = document.createElement('canvas');
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight;
+    canv.id = 'someId';
+    document.body.append(canv);
+    let context = canv.getContext("2d");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canv.width, canv.height);
+    context.font = "50px Arial";
+    context.textAlign = "center";
+    context.fillStyle = "white";
+    context.fillText("Please, rotate your screen", window.innerWidth / 2, window.innerHeight / 2);
+    console.log(window.innerWidth + " " + window.innerHeight)
+} else {
+    new Phaser.Game(config);
+}
 
 
