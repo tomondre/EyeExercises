812d9f4b3aa24f63960ea30961f1c5de5275db6f
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-30 17:54:02 +0100

    Jumping Column now displays rotate screen message when being played on vertical

--- JumpingColumn/src/main.js
+++ JumpingColumn/src/main.js
@@ -3,10 +3,31 @@ import GameScene from "./scenes/GameScene";
 import LoadScene from "./scenes/LoadScene";
 
 const config = {
-    width: window.innerWidth - 21,
+    width: window.innerWidth,
     height: window.innerHeight,
     type: Phaser.AUTO,
     scene: [LoadScene, GameScene]
 }
 
-const game = new Phaser.Game(config);
+window.addEventListener("orientationchange", () => {
+    location.reload();
+});
+
+if (window.innerWidth < window.innerHeight) {
+    var canv = document.createElement('canvas');
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight;
+    canv.id = 'someId';
+    document.body.append(canv);
+    let context = canv.getContext("2d");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canv.width, canv.height);
+    context.font = "50px Arial";
+    context.textAlign = "center";
+    context.fillStyle = "white";
+    context.fillText("Please, rotate your screen", window.innerWidth / 2, window.innerHeight / 2);
+    console.log(window.innerWidth + " " + window.innerHeight)
+
+} else {
+    new Phaser.Game(config);
+}
