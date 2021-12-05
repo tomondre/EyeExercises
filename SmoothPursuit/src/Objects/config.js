5178f4be142a60f869ba77073255e436acd00ba0
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-12-05 13:46:05 +0100

    Creating config as a separate class

---
+++ SmoothPursuit/src/Objects/config.js
@@ -0,0 +1,16 @@
+export const config = {
+    game: {
+        frameRate: 50
+    },
+    levels: {
+        levels: 4,
+        difficulties: 6,
+        subDifficulties: 4
+    },
+    difficulties: [
+        {
+            index : 0,
+            subjectSpeedPerFrame : 10
+        },
+    ],
+}
