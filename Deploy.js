b2b8c2aad662d54967a8f3bc7c2a608fa5f49b6b
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-12-20 18:39:21 +0100

    Adding deploy script

--- Deploy.js
+++ Deploy.js
@@ -4,29 +4,39 @@ let AnalgyphExerciseSettings = {
     dest: 'Analgyph',
     branch: 'gh-pages'
 };
-ghpages.publish('AnalgyphExercise', AnalgyphExerciseSettings, () => { console.log("Analgyph deployed")});
+ghpages.publish('AnalgyphExercise', AnalgyphExerciseSettings, () => {
+    console.log("Analgyph deployed")
+}).then(() => {
+    let FootballGameSettings = {
+        dest: 'Football',
+        branch: 'gh-pages'
+    };
+    ghpages.publish('FootballGame/dist', FootballGameSettings, () => {
+        console.log("Football deployed")
+    }).then(() => {
+        let JumpingColumnSettings = {
+            dest: 'JumpingColumn',
+            branch: 'gh-pages'
+        };
+        ghpages.publish('JumpingColumn/dist', JumpingColumnSettings, () => {
+            console.log("JumpingColumn deployed")
+        }).then(() => {
+            let MazeSettings = {
+                dest: 'Maze',
+                branch: 'gh-pages'
+            };
+            ghpages.publish('Maze/dist', MazeSettings, () => {
+                console.log("Maze deployed")
+            }).then(() => {
+                let SmoothPursuitSettings = {
+                    dest: 'SmoothPursuit',
+                    branch: 'gh-pages'
+                };
 
-let FootballGameSettings = {
-    dest: 'Football',
-    branch: 'gh-pages'
-};
-ghpages.publish('FootballGame/dist', FootballGameSettings, () => { console.log("Football deployed")});
-
-let JumpingColumnSettings = {
-    dest: 'JumpingColumn',
-    branch: 'gh-pages'
-};
-ghpages.publish('JumpingColumn/dist', JumpingColumnSettings, () => { console.log("JumpingColumn deployed")});
-
-let MazeSettings = {
-    dest: 'Maze',
-    branch: 'gh-pages'
-};
-ghpages.publish('Maze/dist', MazeSettings, () => { console.log("Maze deployed")});
-
-let SmoothPursuitSettings = {
-    dest: 'SmoothPursuit',
-    branch: 'gh-pages'
-};
-
-ghpages.publish('SmoothPursuit/dist', SmoothPursuitSettings, () => { console.log("SmoothPursuit deployed")});
+                ghpages.publish('SmoothPursuit/dist', SmoothPursuitSettings, () => {
+                    console.log("SmoothPursuit deployed")
+                }).then(() => console.log("Great Success!"));
+            });
+        });
+    });
+});
