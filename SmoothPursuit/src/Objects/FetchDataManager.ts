d9053833ae380f7539cfcd6407ea23d0bf00d637
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-29 13:35:27 +0100

    Default value now contains 2.5 minute for each eye

--- SmoothPursuit/src/Objects/FetchDataManager.ts
+++ SmoothPursuit/src/Objects/FetchDataManager.ts
@@ -35,7 +35,7 @@ export default class FetchDataManager {
         let time = window.localStorage.getItem(gameCode + "/Time/" + eye.toString());
         if (time === null)
         {
-            return -1;
+            return 150;
         }
         return parseInt(time);
     }
