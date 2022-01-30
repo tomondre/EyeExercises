e265f3268dcf5e93d46e35a8fcefe966682171f0
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-30 11:46:00 +0100

    Removing comments

--- Maze/src/main.js
+++ Maze/src/main.js
@@ -1,17 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Videos
-// https://youtu.be/HyK_Q5rrcr4
-// https://youtu.be/D8UgRyRnvXU
-// https://youtu.be/8Ju_uxJ9v44
-// https://youtu.be/_p5IH0L63wo
-
-// Depth-first search
-// Recursive backtracker
-// https://en.wikipedia.org/wiki/Maze_generation_algorithm
-
 import {config} from "./config";
 import p5 from "p5";
 import Game from "./Game";
